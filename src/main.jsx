9ce5924bc43aa1c3bbd839652ffa4daf413f000b
Louis Fernando <ferlouis55@gmail.com>
2025-02-20 00:42:53 +0700

    Ready to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,10 @@ import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
+if (import.meta.env.NODE_ENV === 'production') disableReactDevTools()
+  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
