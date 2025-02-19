42143395a2d6b95a2e214100817361b0d94249e0
Louis Fernando <ferlouis55@gmail.com>
2025-02-20 01:08:04 +0700

    Deploy to vercel

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://technotes-frontend.onrender.com',
+    baseUrl: 'https://technotes-frontend.vercel.app',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
