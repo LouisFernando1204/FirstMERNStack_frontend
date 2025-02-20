e98ef54274f7d0028fdae9bb1b0e7cd3484add9c
Louis Fernando <ferlouis55@gmail.com>
2025-02-20 09:26:08 +0700

    Checklist all user stories

--- src/features/auth/Welcome.jsx
+++ src/features/auth/Welcome.jsx
@@ -18,9 +18,9 @@ const Welcome = () => {
 
             <h1>Welcome {username}!</h1>
 
-            <p><Link to="/dash/notes">View TechMate</Link></p>
+            <p><Link to="/dash/notes">View Notes</Link></p>
 
-            <p><Link to="/dash/notes/new">Add New techNote</Link></p>
+            <p><Link to="/dash/notes/new">Add New Note</Link></p>
 
             {(isManager || isAdmin) && <p><Link to="/dash/users">View User Settings</Link></p>}
 
